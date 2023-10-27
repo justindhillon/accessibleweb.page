71f4e9eb6fdaea3fb06045fcb1c12b0ef613a744
justindhillon <justin.singh.dhillon@gmail.com>
2023-10-26 19:42:17 -0700

    temp

--- index.py
+++ index.py
@@ -1,8 +1,7 @@
 import flask
 import requests
-import urllib.parse
+import dotenv
 from flask import request
-import re
 
 app = flask.Flask(__name__)
 
@@ -18,10 +17,10 @@ def accessibility_settings(url):
 def main_page(): 
     return flask.render_template('index.html')
 
-# Code from https://github.com/wasi-master/13ft
-@app.route("/", defaults={"path": ""})
-@app.route('/<path:path>', methods=["GET"])
-def get_article(path):
+@app.route('/', defaults={'path': ''})
+@app.route('/<path>')
+def redirect_to_API_HOST(path):
+    # Redirects if url is a webpage
     full_url = request.url
     parts = full_url.split('/',4)
     if len(parts) >= 5:
@@ -32,7 +31,25 @@ def get_article(path):
             return str(e), 400
         except e:
             raise e
-    else:
-        return "Invalid URL", 400
+    
+    res = requests.request(  # ref. https://stackoverflow.com/a/36601467/248616
+        method          = request.method,
+        url             = request.url.replace(request.host_url, f'{API_HOST}/'),
+        headers         = {k:v for k,v in request.headers if k.lower() != 'host'}, # exclude 'host' header
+        data            = request.get_data(),
+        cookies         = request.cookies,
+        allow_redirects = False,
+    )
+
+    #region exlcude some keys in :res response
+    excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']  #NOTE we here exclude all "hop-by-hop headers" defined by RFC 2616 section 13.5.1 ref. https://www.rfc-editor.org/rfc/rfc2616#section-13.5.1
+    headers          = [
+        (k,v) for k,v in res.raw.headers.items()
+        if k.lower() not in excluded_headers
+    ]
+    #endregion exlcude some keys in :res response
+
+    response = Response(res.content, res.status_code, headers)
+    return response
 
 app.run()
