a415b384cec669e8848110befde1556c04872960
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-11-06 21:05:48 -0800

    made button only work on click

--- src/settings/public/index.js
+++ src/settings/public/index.js
@@ -18,3 +18,12 @@ function addFilter(property) {
         document.body.classList.add(property);
     }
 }
+
+document.querySelector(".dropbtn").addEventListener("click", function() {
+    var dropdownContent = document.querySelector(".dropdown-content");
+    if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none"; // Close the dropdown
+    } else {
+        dropdownContent.style.display = "block"; // Open the dropdown
+    }
+});
